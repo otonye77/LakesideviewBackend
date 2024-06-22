8e803e071a9b22959a76fe1e209f3dcb4ea8c4d6
otonye77 <oamietubodie@gmail.com>
2024-06-22 16:47:16 +0100

    entities for rooms and bookedrooms

--- src/main/java/com/lakeside/lakesidehotel/models/BookedRoom.java
+++ src/main/java/com/lakeside/lakesidehotel/models/BookedRoom.java
@@ -1,17 +1,40 @@
 package com.lakeside.lakesidehotel.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    @Column(name = "check_In")
     private LocalDate checkInDate;
+    @Column(name = "check_Out")
     private LocalDate checkOutDate;
+    @Column(name = "guest_FullName")
     private String guestFullName;
+    @Column(name = "guest_Email")
     private String guestEmail;
+    @Column(name = "children")
     private int NumOfChildren;
+    @Column(name = "adults")
     private int NumOfAdults;
+    @Column(name = "total_guest")
     private int totalNumOfGuest;
+    @Column(name = "confirmation_Code")
     private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
     private Room room;
 
     public void calculateNumberOfGuest(){
@@ -26,5 +49,9 @@ public class BookedRoom {
     public void setNumOfChildren(int numOfChildren) {
         NumOfChildren = numOfChildren;
         calculateNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
     }
 }
--- src/main/java/com/lakeside/lakesidehotel/models/Room.java
+++ src/main/java/com/lakeside/lakesidehotel/models/Room.java
@@ -1,14 +1,26 @@
 package com.lakeside.lakesidehotel.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private Boolean isBooked = false;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room(){
