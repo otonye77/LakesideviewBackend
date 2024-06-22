e490909843abce862670fafb8756401e82d6dc16
otonye77 <oamietubodie@gmail.com>
2024-06-22 16:31:48 +0100

    entities for rooms and bookedrooms

--- src/main/java/com/lakeside/lakesidehotel/models/BookedRoom.java
+++ src/main/java/com/lakeside/lakesidehotel/models/BookedRoom.java
@@ -1,4 +1,30 @@
 package com.lakeside.lakesidehotel.models;
 
+import java.time.LocalDate;
+
 public class BookedRoom {
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int NumOfChildren;
+    private int NumOfAdults;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private Room room;
+
+    public void calculateNumberOfGuest(){
+        this.totalNumOfGuest = this.NumOfAdults + this.NumOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        NumOfAdults = numOfAdults;
+        calculateNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        NumOfChildren = numOfChildren;
+        calculateNumberOfGuest();
+    }
 }
--- src/main/java/com/lakeside/lakesidehotel/models/Room.java
+++ src/main/java/com/lakeside/lakesidehotel/models/Room.java
@@ -1,4 +1,17 @@
 package com.lakeside.lakesidehotel.models;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private Boolean isBooked = false;
+    private List<BookedRoom> bookings;
+
+    public Room(){
+        this.bookings = new ArrayList<>();
+    }
 }
