8be92eb64418b51149fd79168455e076c015a1b2
otonye77 <oamietubodie@gmail.com>
2024-06-25 00:48:16 +0100

    get room types

--- src/main/java/com/lakeside/lakesidehotel/repository/RoomRepository.java
+++ src/main/java/com/lakeside/lakesidehotel/repository/RoomRepository.java
@@ -2,6 +2,11 @@ package com.lakeside.lakesidehotel.repository;
 
 import com.lakeside.lakesidehotel.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
 }
